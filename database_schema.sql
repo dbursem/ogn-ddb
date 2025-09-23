f1a74e3fe28fa3161702683a1707824b2aef84db
Sebastien Chaumontet <sebastien@chaumontet.net>
2025-09-23 23:54:35 +0200

    Update database_schema.sql with latest version
    Including event to cleanup tmp accounts.

--- database_schema.sql
+++ database_schema.sql
@@ -1,46 +1,55 @@
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DELIMITER ;;
+
+DROP EVENT IF EXISTS `Clean tmp accounts`;;
+CREATE EVENT `Clean tmp accounts` ON SCHEDULE EVERY 1 DAY STARTS '2024-12-08 19:53:01' ON COMPLETION PRESERVE ENABLE DO DELETE FROM `tmpusers` WHERE `tusr_time` < UNIX_TIMESTAMP(DATE_SUB(now(),interval 10 day));;
+
+DELIMITER ;
 
 DROP TABLE IF EXISTS `aircrafts`;
-DROP TABLE IF EXISTS `devices`;
-DROP TABLE IF EXISTS `tmpusers`;
-DROP TABLE IF EXISTS `users`;
-
 CREATE TABLE `aircrafts` (
-  `ac_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `ac_id` smallint unsigned NOT NULL AUTO_INCREMENT,
   `ac_type` varchar(32) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
-  `ac_cat` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `ac_cat` tinyint unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`ac_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 
+DROP TABLE IF EXISTS `devices`;
 CREATE TABLE `devices` (
-  `dev_id` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
-  `dev_type` tinyint(4) NOT NULL DEFAULT '2',
-  `dev_actype` smallint(5) unsigned NOT NULL,
-  `dev_acreg` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
-  `dev_accn` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
-  `dev_userid` mediumint(8) unsigned NOT NULL,
-  `dev_notrack` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `dev_noident` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `dev_updatetime` bigint(20) NOT NULL,
-  UNIQUE KEY `dev_id` (`dev_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `dev_id` varchar(6) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `dev_type` tinyint NOT NULL DEFAULT '2',
+  `dev_actype` smallint unsigned NOT NULL,
+  `dev_acreg` varchar(7) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `dev_accn` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `dev_userid` mediumint unsigned NOT NULL,
+  `dev_notrack` tinyint unsigned NOT NULL DEFAULT '0',
+  `dev_noident` tinyint unsigned NOT NULL DEFAULT '0',
+  `dev_updatetime` bigint NOT NULL,
+  UNIQUE KEY `dev_id` (`dev_id`),
+  KEY `dev_notrack` (`dev_notrack`),
+  KEY `dev_noident` (`dev_noident`),
+  KEY `dev_actype` (`dev_actype`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 
+DROP TABLE IF EXISTS `tmpusers`;
 CREATE TABLE `tmpusers` (
-  `tusr_adress` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `tusr_pw` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `tusr_adress` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `tusr_pw` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `tusr_validation` varchar(128) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
-  `tusr_time` bigint(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `tusr_time` bigint NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `usr_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `usr_adress` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `usr_pw` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`usr_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `usr_id` mediumint unsigned NOT NULL AUTO_INCREMENT,
+  `usr_adress` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `usr_pw` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`usr_id`),
+  UNIQUE KEY `usr_adress` (`usr_adress`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
