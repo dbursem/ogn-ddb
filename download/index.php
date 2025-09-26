3baca7f1e6fdfd09b2bd76ba8a9ca775d1d30cbb
David Ursem <git@dbursem.nl>
2025-09-26 11:24:53 +0200

    add a simple caching mechanism to the download page

--- download/index.php
+++ download/index.php
@@ -1,106 +1,154 @@
 <?php
 
-# sleep randomly up to 500ms to dispatch queries
-usleep(rand(0,500000));
+require_once '../vendor/autoload.php';
 
-include '../sql.php';
-$dbh = Database::connect();
-$devtype = array(1 => 'I', 2 => 'F', 3 => 'O');
+$cacheDir  = '/tmp/ddb-cache/';
+$cacheHash = getCacheHash();
+$cacheFile = $cacheDir . $cacheHash;
+$jsonData  = null;
 
-$t = !empty($_GET['t']);
-$actype = $t ? ', ac_cat AS aircraft_type ' : '';
-
-$params = array();
-$filter = array();
-
-if (!empty($_GET['device_id'])) {
-    $regs = explode(',', $_GET['device_id']);
-    $qm = implode(',', array_fill(0, count($regs), '?'));
-    $filter[] = 'dev_id IN ('.$qm.')';
-    $params = array_merge($params, $regs);
+if (file_exists($cacheFile) && filemtime($cacheFile) > (new DateTime('-10 minutes'))->format('U')) {
+    $jsonData = file_get_contents($cacheFile);
 }
-if (!empty($_GET['from_id'])) {
-    $filter[] = 'dev_id >= ?';
-    array_push($params, $_GET['from_id']);
-}
-if (!empty($_GET['till_id'])) {
-    if (!empty($_GET['from_id'])) {
-      $filter[count($filter)-1] .= ' AND dev_id <= ?';
-      array_push($params, $_GET['till_id']);
-    } else {
-      $filter[] = 'dev_id <= ?';
-      array_push($params, $_GET['till_id']);
+
+if (!$jsonData) {
+    try {
+        $data = ['devices' => doQuery()];
+        $jsonData = json_encode($data, JSON_THROW_ON_ERROR);
+    } catch (JsonException $e) {
+        header('HTTP/1.0 501 Not Implemented');
+        echo '{"errormsg":"' . $e->getMessage() . '"}';
+        exit;
     }
-}
-if (!empty($_GET['registration'])) {
-    $regs = explode(',', $_GET['registration']);
-    $qm = implode(',', array_fill(0, count($regs), '?'));
-    $filter[] = ' ( dev_acreg IN ('.$qm.') AND dev_notrack = 0 AND dev_noident = 0 ) ';
-    $params = array_merge($params, $regs);
-}
-if (!empty($_GET['cn'])) {
-    $regs = explode(',', $_GET['cn']);
-    $qm = implode(',', array_fill(0, count($regs), '?'));
-    $filter[] = ' ( dev_accn IN ('.$qm.') AND dev_notrack = 0 AND dev_noident = 0 ) ';
-    $params = array_merge($params, $regs);
-}
-if (count($filter)) {
-    $filterstring = 'WHERE '.implode(' OR ', $filter);
-} else {
-    $filterstring = '';
+    file_put_contents($cacheFile . '.tmp', $jsonData);
+    rename($cacheFile . '.tmp', $cacheFile);
 }
 
-$sql = 'SELECT
-    dev_type AS device_type,
-    dev_id AS device_id,
-
-    IF(!dev_notrack AND !dev_noident,ac_type,"" ) AS aircraft_model,
-    IF(!dev_notrack AND !dev_noident,dev_acreg,"") AS registration,
-    IF(!dev_notrack AND !dev_noident,dev_accn,"") AS cn,
-
-    IF(!dev_notrack,"Y","N") AS tracked,
-    IF(!dev_noident,"Y","N") AS identified
-    '.$actype.'
-    FROM devices
-    LEFT JOIN aircrafts
-    ON dev_actype = ac_id
-    '.$filterstring.'
-    ORDER BY dev_id ASC';
-$stmt = $dbh->prepare($sql);
-$stmt->execute($params);
-
-$output['devices'] = array();
-
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $row['device_type'] = $devtype[$row['device_type']];
-    $output['devices'][] = $row;
-}
 
 if (!empty($_GET['j']) || !empty($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] == 'application/json') {
+    outputJson($jsonData);
+} else {
+    outputCsv(json_decode($jsonData, true));
+}
+
+
+function getCacheHash(): string
+{
+    $filters = [
+        't',
+        'device_id',
+        'from_id',
+        'till_id',
+        'registration',
+        'cn',
+    ];
+
+    $cacheIdentifier = '';
+    foreach ($filters as $filter) {
+        $cacheIdentifier .= $_GET[$filter];
+    }
+    return md5($cacheIdentifier);
+}
+
+function doQuery(): array
+{
+    # sleep randomly up to 500ms to dispatch queries
+    usleep(rand(0,500000));
+    
+    $dbh = Database::connect();
+
+    $t = !empty($_GET['t']);
+    $actype = $t ? ', ac_cat AS aircraft_type ' : '';
+
+    $params = array();
+    $filter = array();
+
+    if (!empty($_GET['device_id'])) {
+        $regs = explode(',', $_GET['device_id']);
+        $qm = implode(',', array_fill(0, count($regs), '?'));
+        $filter[] = 'dev_id IN ('.$qm.')';
+        $params = array_merge($params, $regs);
+
+    }
+
+    if (!empty($_GET['from_id'])) {
+        $filter[] = 'dev_id >= ?';
+        array_push($params, $_GET['from_id']);
+    }
+
+    if (!empty($_GET['till_id'])) {
+        if (!empty($_GET['from_id'])) {
+        $filter[count($filter)-1] .= ' AND dev_id <= ?';
+        array_push($params, $_GET['till_id']);
+        } else {
+        $filter[] = 'dev_id <= ?';
+        array_push($params, $_GET['till_id']);
+        }
+    }
+
+    if (!empty($_GET['registration'])) {
+        $regs = explode(',', $_GET['registration']);
+        $qm = implode(',', array_fill(0, count($regs), '?'));
+        $filter[] = ' ( dev_acreg IN ('.$qm.') AND dev_notrack = 0 AND dev_noident = 0 ) ';
+        $params = array_merge($params, $regs);
+    }
+
+    if (!empty($_GET['cn'])) {
+        $regs = explode(',', $_GET['cn']);
+        $qm = implode(',', array_fill(0, count($regs), '?'));
+        $filter[] = ' ( dev_accn IN ('.$qm.') AND dev_notrack = 0 AND dev_noident = 0 ) ';
+        $params = array_merge($params, $regs);
+    }
+
+    if (count($filter)) {
+        $filterstring = 'WHERE ' . implode(' OR ', $filter);
+    } else {
+        $filterstring = '';
+    }
+
+    $sql = <<<eot
+    SELECT
+        CASE dev_type WHEN 1 THEN "I" WHEN 2 THEN "F" WHEN 3 THEN "O" ELSE "" END AS device_type,
+        dev_id AS device_id,
+        IF(!dev_notrack AND !dev_noident, ac_type, "" ) AS aircraft_model,
+        IF(!dev_notrack AND !dev_noident, dev_acreg, "") AS registration,
+        IF(!dev_notrack AND !dev_noident, dev_accn, "") AS cn,
+        IF(!dev_notrack,"Y","N") AS tracked,
+        IF(!dev_noident,"Y","N") AS identified
+        $actype
+        FROM devices
+        LEFT JOIN aircrafts
+        ON dev_actype = ac_id
+        $filterstring
+        ORDER BY dev_id ASC
+eot;
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($params);
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function outputJson(string $output): never
+{
     // Allow from any origin
     header('Access-Control-Allow-Headers: Content-Type');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    $json = json_encode($output); 
-    if ($json) 
-	echo $json; 
-    else 
-	{
-        header('HTTP/1.0 501 Not Implemented');
-	echo '{"errormsg":"'.json_last_error_msg().'"}';
-	exit;
-	}
-} else {
+	echo $output; 
+    exit;
+}
+
+function outputCsv(array $output): never
+{
     header('Content-Type: text/plain; charset="UTF-8"');
-    echo '#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED';
-    if ($t) {
-        echo ',AIRCRAFT_TYPE';
-    }
+    $keys = array_keys($output['devices'][0] ?? []);
+    echo '#' . implode(',', array_map('strtoupper', $keys));
     echo "\r\n";
     foreach ($output['devices'] as $row) {
         echo "'";
         echo implode("','", $row);
         echo "'\r\n";
     }
+    exit;
 }
